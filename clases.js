f65c730c828cef8a1c66618a6408c33efc9af1df
Maximiliano Salomon <maxi.salomon@gmail.com>
2022-03-10 22:02:15 -0300

    fin

--- clases.js
+++ clases.js
@@ -2,8 +2,8 @@ class Usuario {
   constructor(nombre, apellido, libros, mascotas) {
     this.nombre = nombre;
     this.apellido = apellido;
-    this.libros = [{}];
-    this.mascotas = [];
+    this.libros = [{ nombre: "libro1", autor: "autor1" }];
+    this.mascotas = ["perro"];
   }
   getFullName() {
     console.log(`Nombre completo: ${this.nombre} ${this.apellido}`);
@@ -14,13 +14,26 @@ class Usuario {
   countMascota() {
     console.log(`Cantidad de mascotas: ${this.mascotas.length}`);
   }
-  addBook() {
-    console.log();
+  addBook(libro) {
+    return this.libros.push(libro);
   }
-  getBookNames() {}
+  getBookNames() {
+    for (const libro of this.libros) {
+      console.log(libro.nombre);
+      // this.libros.forEach((libro) => console.log(libro.nombre));
+    }
+  }
 }
 
+//Creo usuario
 const usuario1 = new Usuario("Maximiliano", "Salomon");
+//Muestro el nombre complete del usuario x consola
 usuario1.getFullName();
+//Agrego una nueva mascota
+usuario1.addMascota("gato");
+//Muestro la cantidad de mascotas x consola
 usuario1.countMascota();
-addMascota("ruholaben");
+//Agrego un nuevo libro
+usuario1.addBook({ nombre: "libro2", autor: "autor2" });
+//Muestro el nombre de todos los libros x consola
+usuario1.getBookNames();
